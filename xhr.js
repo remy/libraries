68119f704bde70d142e0dfefa98cb2431760550c
Remy Sharp <remy@leftlogic.com>
2013-04-23 19:08:29 +0200

    Create xhr.js

---
+++ xhr.js
@@ -0,0 +1,36 @@
+function request(type, url, opts, callback) {
+  var xhr = new XMLHttpRequest(),
+      fd;
+
+  if (typeof opts === 'function') {
+    callback = opts;
+    opts = null;
+  }
+
+  xhr.open(type, url);
+
+  if (type === 'POST' && opts) {
+    fd = new FormData();
+
+    for (var key in opts) {
+      fd.append(key, JSON.stringify(opts[key]));
+    }
+  }
+
+  xhr.setRequestHeader('x-requested-with', 'XMLHttpRequest');
+
+  xhr.onload = function () {
+    callback(null, JSON.parse(xhr.response));
+  };
+
+  xhr.onerror = function () {
+    callback(true);
+  };
+
+  xhr.send(opts ? fd : null);
+}
+
+var get = request.bind(this, 'GET');
+var post = request.bind(this, 'POST');
+
+// usage: get('/foo', function (err, data) { ... })
